c9c21860d89352939dbf40ce7332ed17b175c68b
Agustin Daniel Messina <agustindmessina@gmail.com>
2022-12-12 21:54:31 -0300

    Navbar and Layout

--- themes/dark-theme.ts
+++ themes/dark-theme.ts
@@ -15,6 +15,15 @@ export const darkTheme = createTheme({
   },
 
   components: {
-    
+    MuiAppBar: {
+      defaultProps: {
+        elevation: 0
+      },
+      styleOverrides: {
+        root: {
+          backgroundColor: '#4a148c'
+        }
+      }
+    }
   }
 });
